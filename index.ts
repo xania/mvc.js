b2e06868595bff5649db703e328975a7be61c2f0
xania <ibrahim.bensalah@gmail.com>
2019-03-18 17:01:16 +0100

    compile fix

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export * from "src/action"
-export * from "src/browser-router"
-export * from "src/chain-cache"
+export * from "./src/action"
+export * from "./src/browser-router"
+export * from "./src/chain-cache"
